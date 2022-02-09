ebef165fecd906a5bfcf4df19dab04cc129c32ea
Martin Greksa <martin.greksaa@gmail.com>
2022-02-09 20:46:32 +0100

    Update word_replacer.py

--- word_replacer.py
+++ word_replacer.py
@@ -1,1 +1,13 @@
+sentence=input("Enter your sentence here: ")
+print("Your sentence is: " + sentence)
 
+question=input("Did you typed your question right? ")
+
+if question.lower() !="no":
+    print("Okay nice to hear that! Have a good day! And see you when you make a mistake in typing haha :) ")
+    quit()
+else:
+    print("Okay lets replace a word in your sentence")
+    word1=input("Enter the word to replace: ")
+    word2=input("Enter the word to replace with: ")
+    print("You new sentence is: " + sentence.replace(word1,word2))
